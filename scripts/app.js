669e389803188813106c9ae5c54a403ff0894283
Axel Broeglin <axelbroeglin@gmail.com>
2022-05-04 21:09:56 +0100

    Button generates a new advice

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,29 @@
+//Gets the placeholder for the Advice ID
+let adviceId = document.getElementById('advice-id');
+//Gets the placeholder for the Advice
+let advice = document.getElementById('advice');
+//Gets the button for a new advice
+let newAdvice = document.getElementById('dice-container');
+
+
+//First request to receive the advice and its ID when page is loaded
 fetch('http://api.adviceslip.com/advice')
 .then(function(resp){
     return resp.json();
 })    
 .then (function(data){
-    let adviceId = document.getElementById('advice-id');
-    let adivce = document.getElementById('advice');
-    console.log(adviceId)
     adviceId.innerText = 'ADVICE ' + data.slip.id;
     advice.innerText = '"' + data.slip.advice + '"';
 }) 
-    
 
-
-    
+//On click on the button, new request to get a new advice
+newAdvice.onclick = () => {
+    fetch('http://api.adviceslip.com/advice')
+    .then(function(resp){
+        return resp.json();
+    })    
+    .then (function(data){
+        adviceId.innerText = 'ADVICE ' + data.slip.id;
+        advice.innerText = '"' + data.slip.advice + '"';
+    })
+}
