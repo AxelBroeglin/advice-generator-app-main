affec492687597fdd4cbc07cce8c039754841c9d
Axel Broeglin <axelbroeglin@gmail.com>
2022-05-04 18:11:47 +0100

    App shows advice id and advice

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,14 @@ fetch('http://api.adviceslip.com/advice')
 .then(function(resp){
     return resp.json();
 })    
-.then(data => console.log(data.slip.id, data.slip.advice));
+.then (function(data){
+    let adviceId = document.getElementById('advice-id');
+    let adivce = document.getElementById('advice');
+    console.log(adviceId)
+    adviceId.innerText = 'ADVICE ' + data.slip.id;
+    advice.innerText = '"' + data.slip.advice + '"';
+}) 
+    
 
 
     
